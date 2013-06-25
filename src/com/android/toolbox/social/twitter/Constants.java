4ee58c81d2e22613ecc94eb57ebdd019e36f0561
gomino <abezzarg@gmail.com>
2013-06-25 19:13:18 +0200

    added twitter Constants

---
+++ src/com/android/toolbox/social/twitter/Constants.java
@@ -0,0 +1,14 @@
+package com.android.toolbox.social.twitter;
+
+/**
+ * @author gomino (amine.bezzarga@labgency.com)
+ */
+public class Constants {
+
+	public static final String 	REQUEST_URL 			= "http://api.twitter.com/oauth/request_token";
+	public static final String 	ACCESS_URL 				= "http://api.twitter.com/oauth/access_token";
+	public static final String 	AUTHORIZE_URL 			= "http://api.twitter.com/oauth/authorize";
+	public static final String 	TWITTER_LOGOUT_URL 		= "http://api.twitter.com/logout";
+	
+	public static final String	OAUTH_CALLBACK_SCHEME	= "x-oauthflow-twitter";
+}
